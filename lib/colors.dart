9307a7e36335b3d4877e0dba16be0abf8149ea02
Aditya Sharma <aditya.sharma.cms17@gmail.com>
2021-09-17 13:28:10 +0530

    created-homescreen

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xffFFFFFF);
+const Color onPrimary = Color(0xff000000);
+const Color secondary = Color(0xff5ECDDA);
+const Color accentColor = Color(0xff4689ED);
+const Color backgroundColor = Color(0xff3C3535);
+const Color buttonGrey = Color(0xffDADADA);
+const Color darkGrey = Color(0xffA6ABBD);
